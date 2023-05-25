6bcef827705838163b4b4ea5ca6c86df54738e81
Olai Solheim <olai.solheim@spk.no>
2023-05-25 19:05:18 +0200

    kopierer til Oppgave 4

---
+++ oppgave-04/src/test/kotlin/no/spk/workshop/kotlin/LambdasTest.kt
@@ -0,0 +1,66 @@
+package no.spk.workshop.kotlin
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+
+internal class LambdasTest {
+
+    @Test
+    fun `Test 1, Lambda som produserer en hilsen for forskjellige navn`() {
+
+        val helloGreeting = Lambdas.helloGreeting()
+
+        helloGreeting("World") shouldBe "Hello, World!"
+        helloGreeting("Kotlin") shouldBe "Hello, Kotlin!"
+    }
+
+    @Test
+    fun `Test 2, Lambda som summerer to tall`() {
+
+        val sum = Lambdas.sum()
+
+        sum(2, 3) shouldBe 5
+        sum(10, 5) shouldBe 15
+    }
+
+    val n = 5
+
+    @Test
+    fun `Test 3, Lambda som bruker variabelen i scopet utenfor (closure)`() {
+
+        val sum = Lambdas.sumWithN()
+
+        sum(2) shouldBe 7
+        sum(10) shouldBe 15
+    }
+
+    @Test
+    fun `Test 4, Lambda som produserer en annen lambda, som multipliserer to tall`() {
+
+        val times = Lambdas.times()
+
+        val doubleIt = times(2)
+        doubleIt(3) shouldBe 6
+        doubleIt(5) shouldBe 10
+
+        val tripleIt = times(3)
+        tripleIt(3) shouldBe 9
+        tripleIt(5) shouldBe 15
+    }
+
+    @Test
+    fun `Test 5, Lambda som multipliserer et tall med én høyere enn et annet tall`() {
+
+        val incrementByOne = { n: Int -> n + 1 }
+        val times = Lambdas.timesWithIncrement()
+
+        val doubleIt = times(2, incrementByOne)
+        doubleIt(3) shouldBe 8
+        doubleIt(5) shouldBe 12
+
+        val tripleIt = times(3, incrementByOne)
+        tripleIt(3) shouldBe 12
+        tripleIt(5) shouldBe 18
+    }
+}
