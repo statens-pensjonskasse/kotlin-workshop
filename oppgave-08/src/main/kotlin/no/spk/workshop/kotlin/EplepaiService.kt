de85b5eee4fb469b03ae632ac56ddcee62fc91e7
Arne Foldvik <ard@spk.no>
2023-05-25 17:34:30 +0200

    La til oppgave 8 med tester

---
+++ oppgave-08/src/main/kotlin/no/spk/workshop/kotlin/EplepaiService.kt
@@ -0,0 +1,12 @@
+package no.spk.workshop.kotlin
+
+class EplepaiService {
+
+    fun bakEplePai(epler: List<Eple>): List<Eplepai> {
+        TODO("Returner en liste over Eplepai-objekter, som har smakerGodt=true kun om eplet er modent.")
+    }
+
+    fun plukkOgBakEplePai(traer: List<Epletre>): List<Eplepai> {
+        TODO("Returner en liste over eplepaier som smaker godt, mappet over fra modne epler i trærne")
+    }
+}
