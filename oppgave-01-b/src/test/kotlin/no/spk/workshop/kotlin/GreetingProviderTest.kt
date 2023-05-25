43b5006303beddb81acba7e06a96cbd1f43ad6f4
Olai Solheim <olai.solheim@spk.no>
2023-05-25 13:34:33 +0200

    kode for Oppgave 1B

--- oppgave-01-b/src/test/kotlin/no/spk/workshop/kotlin/GreetingProviderTest.kt
+++ oppgave-01-b/src/test/kotlin/no/spk/workshop/kotlin/GreetingProviderTest.kt
@@ -8,7 +8,7 @@ internal class GreetingProviderTest {
     @Test
     fun `Provides correct greeting`() {
         val greetingProvider = GreetingProvider()
-        val greeting = greetingProvider.greeting("World")
-        greeting shouldBe "Hello, World!"
+        val greeting = greetingProvider.greeting()
+        greeting shouldBe "Hello, Kotlin!"
     }
 }
