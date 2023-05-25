2b549e916331a56c60f179acc11e95524a547f35
Olai Solheim <olai.solheim@spk.no>
2023-05-25 14:09:34 +0200

    kode for Oppgave 2

---
+++ oppgave-02/src/main/java/no/spk/workshop/kotlin/GreetingProvider.java
@@ -0,0 +1,6 @@
+package no.spk.workshop.kotlin;
+
+public interface GreetingProvider {
+
+    String greeting();
+}
---
+++ oppgave-02/src/main/kotlin/no/spk/workshop/kotlin/KotlinGreetingProvider.kt
@@ -0,0 +1,7 @@
+package no.spk.workshop.kotlin
+
+class KotlinGreetingProvider {
+    /**
+     * TODO skriv tilsvarende kode som i [LegacyGreetingProvider]
+     */
+}
