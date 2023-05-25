0251f4c212647d5986843f90532f3d850d31eafa
Olai Solheim <olai.solheim@spk.no>
2023-05-25 22:50:46 +0200

    døper om Oppgave 1A til Oppgave 1, 1B til 2, 2 til 3, og 3 til 4

--- oppgave-01-b/src/main/java/no/spk/workshop/kotlin/LegacyGreetingProvider.java
+++
@@ -1,9 +0,0 @@
-package no.spk.workshop.kotlin;
-
-public class LegacyGreetingProvider {
-
-    public String greeting() {
-        final String name = new NameProvider().name();
-        return "Hello, " + name + "!";
-    }
-}
