c6fd55dfd1d66a0aca4c5da5136b607dbc5f96cd
Olai Solheim <olai.solheim@spk.no>
2023-05-25 19:46:38 +0200

    fjerner ting

--- oppgave-10/src/test/kotlin/no/spk/workshop/kotlin/DeepThoughtTest.kt
+++ oppgave-10/src/test/kotlin/no/spk/workshop/kotlin/DeepThoughtTest.kt
@@ -1,13 +1,12 @@
 package no.spk.workshop.kotlin
 
-import io.kotest.common.runBlocking
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 internal class DeepThoughtTest {
 
     @Test
-    fun `Gets the answer as fast as possible`() = runBlocking {
+    fun `Gets the answer as fast as possible`() {
 
         val deepThought = DeepThought()
         val answer = deepThought.calculateTheAnswerToLifeTheUniverseAndEverything()
