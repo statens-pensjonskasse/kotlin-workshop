9ba8651fda5ac4eba07a22780ac9d05914cc1a9d
Olai Solheim <olai.solheim@spk.no>
2023-05-25 19:49:51 +0200

    fasit for Oppgave 10

--- oppgave-10/src/test/kotlin/no/spk/workshop/kotlin/DeepThoughtTest.kt
+++ oppgave-10/src/test/kotlin/no/spk/workshop/kotlin/DeepThoughtTest.kt
@@ -1,15 +1,20 @@
 package no.spk.workshop.kotlin
 
+import io.kotest.assertions.timing.eventually
+import io.kotest.common.runBlocking
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import kotlin.time.Duration.Companion.seconds
 
 internal class DeepThoughtTest {
 
     @Test
-    fun `Gets the answer as fast as possible`() {
+    fun `Gets the answer as fast as possible`() = runBlocking {
 
         val deepThought = DeepThought()
-        val answer = deepThought.calculateTheAnswerToLifeTheUniverseAndEverything()
-        answer shouldBe 42
+        eventually(30.seconds) {
+            val answer = deepThought.calculateTheAnswerToLifeTheUniverseAndEverything()
+            answer shouldBe 42
+        }
     }
 }
