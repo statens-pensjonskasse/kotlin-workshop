d08bd3ece1584447695cdd60f030d6aae1eff668
Olai Solheim <olai.solheim@spk.no>
2023-05-25 12:50:01 +0200

    wip

---
+++ oppgave-01-a/src/main/kotlin/no/spk/workshop/kotlin/GreetingProvider.kt
@@ -0,0 +1,8 @@
+package no.spk.workshop.kotlin
+
+class GreetingProvider {
+
+    fun greeting(name: String): String {
+        return "Hi, $name!"
+    }
+}
--- oppgave1/src/main/kotlin/no/spk/workshop/kotlin/Oppgave1.kt
+++
@@ -1,5 +0,0 @@
-package no.spk.workshop.kotlin
-
-fun getGreeting(name: String): String {
-    return "Hi, $name!"
-}
