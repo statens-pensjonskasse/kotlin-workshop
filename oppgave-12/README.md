c63e0d1b5bb7142a7b3c9e385ee1641e45a482a7
Olai Solheim <olai.solheim@spk.no>
2023-06-07 15:48:35 +0200

    fasit for Oppgave 12

--- oppgave-12/README.md
+++ oppgave-12/README.md
@@ -9,8 +9,13 @@ i SPK, hvis du er kjent med den.
 
 Det vi skal mappe til, er flettefelter i et brev, hvor adressen skal legges inn
 sammen med det som ligger der fra før. Flettefeltene er en key/value-struktur,
-eller en `map<String, Any?>` i Kotlin. `Any?` fordi flettefelter kan nestes, og
+eller en `map<String, Any?>` i Kotlin. `Any?` fordi flettefelter kan nestes og
 være `null`.
+
+Utfordringen er at dataklassen vi skal mappe fra og flettefeltene vi skal mappe
+til har ulikt antall adresselinjer tilgjengelig. Flettefeltene har maksimalt fem
+adresselinjer til disposisjon. Felter som ikke får plass, skal derfor kombineres
+med komma. Postnr og -sted skal kombineres til ett felt.
 
 I koden finner du en test som bruker dynamiske/parametriserte tester fra JUnit 5
 til å dekke en hel rekke forskjellige kombinasjoner av adresser. Testen feiler.
--- oppgave-12/src/main/kotlin/no/spk/workshop/kotlin/FlettefeltResolver.kt
+++ oppgave-12/src/main/kotlin/no/spk/workshop/kotlin/FlettefeltResolver.kt
@@ -3,12 +3,40 @@ package no.spk.workshop.kotlin
 class FlettefeltResolver {
 
     fun populerFlettefeltMedAdresse(flettefelt: MutableMap<String, Any?>, adresse: Adresse): MutableMap<String, Any?> {
-        // TODO dine endringer i denne metoden :-)
-        flettefelt["brevhode.adresselinje1"] = "Test Testersen"
-        flettefelt["brevhode.adresselinje2"] = "Testveien 1"
-        flettefelt["brevhode.adresselinje3"] = "1337 Sandvika"
-        flettefelt["brevhode.adresselinje4"] = ""
-        flettefelt["brevhode.adresselinje5"] = ""
+
+        val adresselinjer = listOf(
+            listOf(
+                adresse.navn,
+                adresse.adresselinje1,
+            ),
+            if (adresse.land != null) {
+                listOf(
+                    listOfNotNull(
+                        adresse.adresselinje2,
+                        adresse.adresselinje3,
+                        adresse.adresselinje4,
+                    ).joinToString(separator = ", "),
+                    "${adresse.postnr} ${adresse.poststed}",
+                    adresse.land,
+                )
+            } else {
+                listOf(
+                    adresse.adresselinje2,
+                    listOfNotNull(
+                        adresse.adresselinje3,
+                        adresse.adresselinje4
+                    ).joinToString(separator = ", "),
+                    "${adresse.postnr} ${adresse.poststed}"
+                )
+            }
+        ).flatten().filter { !it.isNullOrEmpty() }
+
+        flettefelt["brevhode.adresselinje1"] = adresselinjer.getOrElse(0) { "" }
+        flettefelt["brevhode.adresselinje2"] = adresselinjer.getOrElse(1) { "" }
+        flettefelt["brevhode.adresselinje3"] = adresselinjer.getOrElse(2) { "" }
+        flettefelt["brevhode.adresselinje4"] = adresselinjer.getOrElse(3) { "" }
+        flettefelt["brevhode.adresselinje5"] = adresselinjer.getOrElse(4) { "" }
+
         return flettefelt
     }
 }
